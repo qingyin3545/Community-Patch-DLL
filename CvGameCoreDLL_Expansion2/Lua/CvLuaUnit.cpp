e3539589d6a84bd7a3471f150c46db4d03891c21
Qingyin <3138584199@qq.com>
2025-03-17 19:21:31 +0800

    migrate: UnitPromotions.MaintenanceCost

--- CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
@@ -679,6 +679,7 @@ void CvLuaUnit::PushMethods(lua_State* L, int t)
 #if defined(MOD_NUCLEAR_WINTER_FOR_SP)
 	Method(GetNuclearWinterProcess);
 #endif
+	Method(GetPromotionMaintenanceCost);
 }
 //------------------------------------------------------------------------------
 const char* CvLuaUnit::GetTypeName()
@@ -6736,3 +6737,4 @@ int CvLuaUnit::lGetNuclearWinterProcess(lua_State* L)
 	return 1;
 }
 #endif
+LUAAPIIMPL(Unit, GetPromotionMaintenanceCost)
