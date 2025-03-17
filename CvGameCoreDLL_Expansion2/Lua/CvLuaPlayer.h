7e93da1972038ce87862c08b93b4ff48344b493c
Qingyin <3138584199@qq.com>
2025-03-17 19:21:10 +0800

    migrate: GetCivUnit && GetCivBuilding

--- CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
@@ -1561,7 +1561,11 @@ protected:
 	LUAAPIEXTN(GetImmigrationInRateFromPolicy, int);
 	LUAAPIEXTN(GetImmigrationOutRateFromPolicy, int);
 #endif
-
+	LUAAPIEXTN(GetCivBuilding, int);
+	LUAAPIEXTN(GetCivUnit, int);
+	LUAAPIEXTN(GetCivBuildingWithDefault, int);
+	LUAAPIEXTN(GetCivUnitWithDefault, int);
+	LUAAPIEXTN(GetCivUnitNowTech, int);
 };
 
 namespace CvLuaArgs
