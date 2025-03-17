5ea82421d254c22a451a71910894d0033aa498ec
Qingyin <3138584199@qq.com>
2025-03-17 19:21:10 +0800

    migrate: GetCivUnit && GetCivBuilding

--- CvGameCoreDLL_Expansion2/CvPlayer.h
+++ CvGameCoreDLL_Expansion2/CvPlayer.h
@@ -2961,6 +2961,7 @@ public:
 #endif
 	BuildingTypes GetCivBuilding(BuildingClassTypes eBuildingClass) const;
 	UnitTypes GetCivUnit(UnitClassTypes eUnitClass, int iFakeSeed = 0) const;
+	UnitTypes GetCivUnitWithDefault(UnitClassTypes eUnitClass) const;
 
 protected:
 	class ConqueredByBoolField
