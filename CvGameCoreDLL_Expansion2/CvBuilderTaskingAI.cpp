90e48b8794de7103dbbcef2e9372e8b59b252ef4
Qingyin <3138584199@qq.com>
2025-03-18 20:32:46 +0800

    changes: Compatibility modification

--- CvGameCoreDLL_Expansion2/CvBuilderTaskingAI.cpp
+++ CvGameCoreDLL_Expansion2/CvBuilderTaskingAI.cpp
@@ -2816,6 +2816,9 @@ int CvBuilderTaskingAI::GetResourceWeight(ResourceTypes eResource, int iQuantity
 			}
 		}
 
+		int iWeightModifier = 100 + pkResource->GetCreateResouceWightModifier();
+		if(iWeightModifier != 100) iValue = iValue * iWeightModifier / 100;
+
 		return iValue;
 	}
 
