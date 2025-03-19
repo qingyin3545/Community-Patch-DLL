00b87e66d34813da854654f48aca40039dcc65ed
touchfishking <3138584199@qq.com>
2025-03-19 09:31:58 +0800

    migrate: SPECIALIST_RESOURCES

--- CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
@@ -1560,6 +1560,9 @@ protected:
 	LUAAPIEXTN(GetImmigrationInRateFromPolicy, int);
 	LUAAPIEXTN(GetImmigrationOutRateFromPolicy, int);
 #endif
+#if defined(MOD_SPECIALIST_RESOURCES)
+	LUAAPIEXTN(GetSpecialistResources, table, eSpecialist);
+#endif
 	LUAAPIEXTN(GetCivBuilding, int);
 	LUAAPIEXTN(GetCivUnit, int);
 	LUAAPIEXTN(GetCivBuildingWithDefault, int);
