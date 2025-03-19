199ff27181837d83e80893205efe7fac0c9604b8
touchfishking <3138584199@qq.com>
2025-03-19 13:59:20 +0800

    migrate: Dual Empire && Diplomatic Marriage

--- CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.h
@@ -1563,6 +1563,10 @@ protected:
 #if defined(MOD_SPECIALIST_RESOURCES)
 	LUAAPIEXTN(GetSpecialistResources, table, eSpecialist);
 #endif
+	LUAAPIEXTN(IsMarriageAccepted, bool);
+	LUAAPIEXTN(GetMarriageCounter, int);
+	LUAAPIEXTN(IsCanDiplomaticMarriage, bool);
+	LUAAPIEXTN(IsAbleToDualEmpire, bool);
 	LUAAPIEXTN(GetCivBuilding, int);
 	LUAAPIEXTN(GetCivUnit, int);
 	LUAAPIEXTN(GetCivBuildingWithDefault, int);
