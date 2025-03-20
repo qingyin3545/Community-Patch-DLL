d7972c435765dce8805164608499358c24d15da8
touchfishking <3138584199@qq.com>
2025-03-20 11:13:24 +0800

    migrate: GameEvents.PlayerSetEra

--- CvGameCoreDLL_Expansion2/CvTeam.cpp
+++ CvGameCoreDLL_Expansion2/CvTeam.cpp
@@ -8814,6 +8814,10 @@ void CvTeam::SetCurrentEra(EraTypes eNewValue)
 			if(kPlayer.isAlive() && kPlayer.getTeam() == GetID())
 			{
 				gDLL->GameplayEraChanged(ePlayer, eNewValue);
+				if (MOD_EVENTS_NEW_ERA) 
+				{
+					GAMEEVENTINVOKE_HOOK(GAMEEVENT_PlayerSetEra, kPlayer.GetID(), eNewValue, !bAlreadyProvided);
+				}
 			}
 		}
 
