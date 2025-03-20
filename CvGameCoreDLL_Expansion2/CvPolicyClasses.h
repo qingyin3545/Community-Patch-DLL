001a5b9e10156dc9ef3326eae4abde8c9b81390a
touchfishking <3138584199@qq.com>
2025-03-20 10:55:05 +0800

    migrate: Policy_CityResources

--- CvGameCoreDLL_Expansion2/CvPolicyClasses.h
+++ CvGameCoreDLL_Expansion2/CvPolicyClasses.h
@@ -483,6 +483,7 @@ public:
 	int GetImmigrationInModifier() const;
 	int GetImmigrationOutModifier() const;
 #endif
+	std::vector<PolicyResourceInfo>& GetCityResources();
 
 private:
 	int m_iTechPrereq;
@@ -879,6 +880,7 @@ private:
 	int m_iImmigrationInModifier = 0;
 	int m_iImmigrationOutModifier = 0;
 #endif
+	std::vector<PolicyResourceInfo> m_vCityResources;
 };
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
