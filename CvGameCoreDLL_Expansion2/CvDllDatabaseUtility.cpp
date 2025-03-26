7734fba8b9d918ec31233eafdd2254b8dbeea19f
touchfishking <3138584199@qq.com>
2025-03-26 09:26:54 +0800

    migrate: LuaFormula

--- CvGameCoreDLL_Expansion2/CvDllDatabaseUtility.cpp
+++ CvGameCoreDLL_Expansion2/CvDllDatabaseUtility.cpp
@@ -410,6 +410,7 @@ bool CvDllDatabaseUtility::PrefetchGameData()
 	PrefetchCollection(GC.getNuclearWinterLevelInfo(), "NuclearWinterLevels");
 	GC.initGlobalNuclearWinterLevels();
 #endif
+	PrefetchCollection(GC.GetLuaFormulaEntries(), "LuaFormula");
 
 	//Copy flavors into string array
 	{
