cfb21baa2e932e5084220e209e4c8744f320683f
touchfishking <3138584199@qq.com>
2025-03-31 14:37:42 +0800

    migrate: Units.BoundImprovement

--- CvGameCoreDLL_Expansion2/CvUnitClasses.h
+++ CvGameCoreDLL_Expansion2/CvUnitClasses.h
@@ -235,6 +235,8 @@ public:
 #if defined(MOD_NUCLEAR_WINTER_FOR_SP)
 	int GetNuclearWinterProcess() const;
 #endif
+	int GetBoundLandImprovement() const;
+	int GetBoundWaterImprovement() const;
 
 private:
 
@@ -438,6 +440,8 @@ private:
 #if defined(MOD_NUCLEAR_WINTER_FOR_SP)
 	int m_iNuclearWinterProcess = 0;
 #endif
+	int m_iBoundLandImprovement = NO_IMPROVEMENT;
+	int m_iBoundWaterImprovement = NO_IMPROVEMENT;
 };
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
