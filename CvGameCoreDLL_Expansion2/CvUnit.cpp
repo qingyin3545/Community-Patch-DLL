30af10912fc0e2933d11a5b1b52a76db1d8379de
touchfishking <3138584199@qq.com>
2025-04-09 11:32:25 +0800

    migrate: UnitPromotions.NoResourcePunishment

--- CvGameCoreDLL_Expansion2/CvUnit.cpp
+++ CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -1704,6 +1704,7 @@ void CvUnit::reset(int iID, UnitTypes eUnit, PlayerTypes eOwner, bool bConstruct
 	m_iOutsideFriendlyLandsInflictDamageChange = 0;
 
 	m_iPromotionMaintenanceCost = 0;
+	m_iNoResourcePunishment = 0;
 
 	if(!bConstructorCall)
 	{
@@ -15959,6 +15960,9 @@ int CvUnit::GetStrategicResourceCombatPenalty() const
 		return 0;
 
 	// Barbs and City-States don't require resources
+	if (IsNoResourcePunishment())
+		return iPenalty;
+
 	CvPlayerAI& kPlayer = GET_PLAYER(getOwner());
 	if (!kPlayer.isMajorCiv())
 		return 0;
@@ -28271,6 +28275,7 @@ void CvUnit::setPromotionActive(PromotionTypes eIndex, bool bNewValue)
 	ChangeOutsideFriendlyLandsInflictDamageChange(iChange * thisPromotion.GetOutsideFriendlyLandsInflictDamageChange());
 
 	ChangePromotionMaintenanceCost(thisPromotion.GetMaintenanceCost() > 0 ? iChange: 0);
+	ChangeIsNoResourcePunishment(thisPromotion.IsNoResourcePunishment() ? iChange : 0);
 
 	if (IsSelected())
 	{
@@ -28991,6 +28996,7 @@ void CvUnit::Serialize(Unit& unit, Visitor& visitor)
 	visitor(unit.m_iOutsideFriendlyLandsInflictDamageChange);
 
 	visitor(unit.m_iPromotionMaintenanceCost);
+	visitor(unit.m_iNoResourcePunishment);
 	visitor(unit.m_iCombatStrengthChangeFromKilledUnits);
 	visitor(unit.m_iRangedCombatStrengthChangeFromKilledUnits);
 }
@@ -34849,6 +34855,16 @@ void CvUnit::ChangePromotionMaintenanceCost(int iValue)
 		m_iPromotionMaintenanceCost += iValue;
 		GET_PLAYER(getOwner()).changeExtraUnitCost(iValue);
 	}
+}
+
+//	--------------------------------------------------------------------------------
+bool CvUnit::IsNoResourcePunishment() const
+{
+	return m_iNoResourcePunishment > 0;
+}
+void CvUnit::ChangeIsNoResourcePunishment(int iChange)
+{
+	m_iNoResourcePunishment += iChange;
 }
 
 //	--------------------------------------------------------------------------------
