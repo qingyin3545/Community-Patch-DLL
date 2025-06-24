42b3823d4095d2155a98815b13970a6501a81365
touchfishking <3138584199@qq.com>
2025-06-24 10:54:19 +0800

    migrate: UnitPromotions_FeatureInvisible

--- CvGameCoreDLL_Expansion2/CvUnit.h
+++ CvGameCoreDLL_Expansion2/CvUnit.h
@@ -2117,6 +2117,11 @@ public:
 	int otherPromotionAttackModifierByUnit(const CvUnit* otherUnit) const;
 	int otherPromotionDefenseModifierByUnit(const CvUnit* otherUnit) const;
 #endif
+	int GetFeatureInvisibleCount(FeatureTypes eIndex) const;
+	bool IsFeatureInvisible(FeatureTypes eIndex) const;
+	void ChangeNumFeatureInvisible(FeatureTypes eIndex, int iChange);
+	bool IsInvisibleInvalid() const;
+
 	bool IsRangeBackWhenDefense() const;
 	void ChangeNumRangeBackWhenDefense(int iChange);
 
@@ -2809,6 +2814,8 @@ private:
 	int m_iSiegeKillCitizensPercent = 0;
 	int m_iSiegeKillCitizensFixed = 0;
 #endif
+	FeatureTypeCounter m_featureInvisibleCount;
+
 	int m_iNumRangeBackWhenDefense = 0;
 	int m_iNumCanSplashDefender = 0;
 
--- CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
@@ -671,6 +671,7 @@ void CvLuaUnit::PushMethods(lua_State* L, int t)
 #ifdef MOD_GLOBAL_PROMOTIONS_REMOVAL
 	Method(ClearSamePlotPromotions);
 #endif
+	Method(IsInvisibleInvalid);
 	Method(GetDamageFixValueToUnit);
 	Method(GetDamageFixValueToCity);
 	Method(GetForcedDamageValue);
@@ -7084,6 +7085,14 @@ int CvLuaUnit::lGetNearbyUnitPromotionBonus(lua_State* L)
 {
 	CvUnit* pkUnit = GetInstance(L);
 	lua_pushinteger(L, GC.GetIndependentPromotion()->GetNearbyUnitPromotionBonus(*pkUnit));
+	return 1;
+}
+//------------------------------------------------------------------------------
+int CvLuaUnit::lIsInvisibleInvalid(lua_State* L)
+{
+	CvUnit* pkUnit = GetInstance(L);
+	const bool bResult = pkUnit->IsInvisibleInvalid();
+	lua_pushboolean(L, bResult);
 	return 1;
 }
 //------------------------------------------------------------------------------
