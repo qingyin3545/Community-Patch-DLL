bd51ce764084cfdaa13092e5006028a0bda56232
touchfishking <3138584199@qq.com>
2025-07-14 09:52:04 +0800

    fix: InvisibleInvalid in SetXY

--- CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
@@ -7052,7 +7052,8 @@ int CvLuaUnit::lGetNearbyUnitPromotionBonus(lua_State* L)
 int CvLuaUnit::lIsInvisibleInvalid(lua_State* L)
 {
 	CvUnit* pkUnit = GetInstance(L);
-	const bool bResult = pkUnit->IsInvisibleInvalid();
+	CvPlot* pkPlot = CvLuaPlot::GetInstance(L, 2);
+	const bool bResult = pkUnit->IsInvisibleInvalid(pkPlot);
 	lua_pushboolean(L, bResult);
 	return 1;
 }
