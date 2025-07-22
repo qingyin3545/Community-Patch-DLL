4e162fdeba055d107e80d5e6ede0e5e9b3283180
touchfishking <3138584199@qq.com>
2025-07-22 10:21:33 +0800

    migrate: DLL_Policies 2

--- CvGameCoreDLL_Expansion2/CvUnit.cpp
+++ CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -13315,7 +13315,7 @@ bool CvUnit::build(BuildTypes eBuild)
 		}
 	}
 
-	int iWorkRateWithMoves = workRate(false);
+	int iWorkRateWithMoves = workRate(false, eBuild);
 
 	int iStartedYet = pPlot->getBuildProgress(eBuild);
 
@@ -15707,7 +15707,7 @@ bool CvUnit::IsWorking() const
 }
 
 //	--------------------------------------------------------------------------------
-int CvUnit::workRate(bool bMax, BuildTypes /*eBuild*/) const
+int CvUnit::workRate(bool bMax, BuildTypes eBuild) const
 {
 	VALIDATE_OBJECT();
 	if (!bMax && !canMove())
@@ -15749,6 +15749,11 @@ int CvUnit::workRate(bool bMax, BuildTypes /*eBuild*/) const
 	else if (kPlayer.isMinorCiv())
 	{
 		Modifiers += GC.getGame().getHandicapInfo().getCityStateWorkRateModifier();
+	}
+
+	if(eBuild != NO_BUILD)
+	{
+		if(GC.getBuildInfo(eBuild)->IsWater()) Modifiers += kPlayer.GetPlayerPolicies()->GetNumericModifier(POLICYMOD_WATER_BUILD_SPEED_MODIFIER);
 	}
 
 	iRate *= Modifiers + 100;
