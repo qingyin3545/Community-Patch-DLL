2fff34a0ddfdbea41fff7287e234cad654daa281
touchfishking <3138584199@qq.com>
2025-07-22 10:21:33 +0800

    migrate: DLL_Policies 2

--- CvGameCoreDLL_Expansion2/CvPlot.cpp
+++ CvGameCoreDLL_Expansion2/CvPlot.cpp
@@ -3352,9 +3352,9 @@ int CvPlot::getBuildTurnsLeft(BuildTypes eBuild, PlayerTypes ePlayer, int iNowEx
 		{
 			if(pLoopUnit->canMove())
 			{
-				iNowBuildRate += pLoopUnit->workRate(false);
+				iNowBuildRate += pLoopUnit->workRate(false, eBuild);
 			}
-			iThenBuildRate += pLoopUnit->workRate(true);
+			iThenBuildRate += pLoopUnit->workRate(true, eBuild);
 		}
 	}
 
@@ -3390,7 +3390,7 @@ int CvPlot::getBuildTurnsTotal(BuildTypes eBuild, PlayerTypes ePlayer) const
 		pUnitNode = nextUnitNode(pUnitNode);
 
 		if (pLoopUnit && pLoopUnit->getBuildType() == eBuild)
-			iBuildRate += pLoopUnit->workRate(true);
+			iBuildRate += pLoopUnit->workRate(true, eBuild);
 	}
 
 	if (iBuildRate == 0)
