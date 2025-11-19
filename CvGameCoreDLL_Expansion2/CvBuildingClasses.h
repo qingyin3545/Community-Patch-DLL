2cc8a239b72b7e8fad651b85d3f994cde84b9b63
touchfishking <3138584199@qq.com>
2025-11-19 11:18:54 +0800

    migrate: DLL_Buildings 2

--- CvGameCoreDLL_Expansion2/CvBuildingClasses.h
+++ CvGameCoreDLL_Expansion2/CvBuildingClasses.h
@@ -742,6 +742,12 @@ public:
 	int GetMinNumReligions() const;
 	int GetTechNoPrereqClasses() const;
 
+	const std::vector<int>& GetBuildingsNeededInCity() const;
+	const std::vector<int>& GetBuildingsNeededGlobal() const;
+	const std::vector<int>& GetPlotAnd() const;
+	const std::vector<int>& GetEmpireResourceAnd() const;
+	const std::vector<int>& GetEmpireResourceOr() const;
+
 	int GetForcedDamageValue() const;
 	int GetChangeDamageValue() const;
 
@@ -1220,6 +1226,12 @@ private:
 	bool m_bOriginalCapitalOnly = false;
 	int m_iMinNumReligions = 0;
 	int m_iTechNoPrereqClasses = NO_TECH;
+
+	std::vector<int> m_piBuildingsNeededInCity;
+	std::vector<int> m_piBuildingsNeededGlobal;
+	std::vector<int> m_piLocalPlotAnds;
+	std::vector<int> m_piEmpireResourceAnds;
+	std::vector<int> m_piEmpireResourceOrs;
 
 	int m_iForcedDamageValue = 0;
 	int m_iChangeDamageValue = 0;
