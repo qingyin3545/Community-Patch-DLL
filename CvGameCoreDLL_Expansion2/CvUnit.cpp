4535f52edde5ad0ccd63d6a45557d1266f474cd3
touchfishking <3138584199@qq.com>
2025-12-09 14:54:37 +0800

    migrate: DLL_Buildings 7

--- CvGameCoreDLL_Expansion2/CvUnit.cpp
+++ CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -16299,6 +16299,7 @@ int CvUnit::GetGenericMeleeStrengthModifier(const CvUnit* pOtherUnit, const CvPl
 	if(kPlayer.isGoldenAge())
 	{
 		iModifier += kPlayer.GetPlayerTraits()->GetGoldenAgeCombatModifier();
+		iModifier += kPlayer.GetGoldenAgeUnitCombatModifier();
 		iModifier += GetGoldenAgeMod();
 	}
 
@@ -17229,6 +17230,7 @@ int CvUnit::GetMaxRangedCombatStrength(const CvUnit* pOtherUnit, const CvCity* p
 	if(kPlayer.isGoldenAge())
 	{
 		iModifier += pTraits->GetGoldenAgeCombatModifier();
+		iModifier += kPlayer.GetGoldenAgeUnitCombatModifier();
 		iModifier += GetGoldenAgeMod();
 	}
 	
