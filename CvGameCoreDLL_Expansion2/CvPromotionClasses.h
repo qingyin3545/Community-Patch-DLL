669f7a3bff8295d44a8fa19506f87a906e8de8dd
Qingyin <3138584199@qq.com>
2025-12-21 07:59:35 +0800

    changes: Compatibility

--- CvGameCoreDLL_Expansion2/CvPromotionClasses.h
+++ CvGameCoreDLL_Expansion2/CvPromotionClasses.h
@@ -446,6 +446,7 @@ public:
 	int GetMaintenanceCost() const;
 	int GetMultipleInitExperience() const;
 	int GetRangeAttackCostModifier() const;
+	int GetNumSlowInEnemyLandCount() const;
 	int GetMovePercentCaptureCity() const;
 	int GetHealPercentCaptureCity() const;
 	int GetLostAllMovesAttackCity() const;
@@ -905,6 +906,7 @@ protected:
 	int m_iMaintenanceCost = 0;
 	int m_iMultipleInitExperience = 0;
 	int m_iRangeAttackCostModifier = 0;
+	int m_iNumSlowInEnemyLandCount = 0;
 	int m_iMovePercentCaptureCity = 0;
 	int m_iHealPercentCaptureCity = 0;
 	int m_iLostAllMovesAttackCity = 0;
