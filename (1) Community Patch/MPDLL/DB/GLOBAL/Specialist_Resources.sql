fbf68bbd11390fdb52822f2f16c5491ad122488c
touchfishking <3138584199@qq.com>
2026-03-18 09:22:39 +0800

    DB from MPDLL

---
+++ (1) Community Patch/MPDLL/DB/GLOBAL/Specialist_Resources.sql
@@ -0,0 +1,8 @@
+create table Specialist_Resources (
+    SpecialistType		text references Specialists(Type),
+    ResourceType		text references Resources(Type),
+    Quantity			integer default 0,
+
+    RequiredPolicyType  text null, -- references Policies(Type)
+    RequiredTechType    text null  -- references Technologies(Type)
+);
